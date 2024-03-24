046457bba31d2809fdfbdf8649039fe491f154a8
TA-800 <taherali0905@gmail.com>
2024-03-24 04:34:00 -0400

    working AST (consistent PrintVisitor spacing)

--- source/AstPrinter.cpp
+++ source/AstPrinter.cpp
@@ -17,10 +17,12 @@ class AstPrinter : public Visitor
     {
         // Call print again on the left and right expressions to recursively print them if they are also other types of
         // expressions. E.g. ( 2 + 3 ) + 4 where 2 + 3 is also a binary expression
-        result += "( " + expr->op.getLexeme();
+        result += "( " + expr->op.getLexeme() + " ";
 
         // Left expression
         setPrintResult(expr->left);
+
+        result += " ";
 
         // Print the right expression
         setPrintResult(expr->right);
@@ -38,24 +40,25 @@ class AstPrinter : public Visitor
         result += " )";
     }
 
+    // No spaces for literals
     void visitLiteralExpr(const Literal *expr) override
     {
         // Token has void *literal that can be string (Type::STRING) or number (Type::NUMBER)
         // If nil
         if (expr->type == TokenInfo::Type::NIL || expr->value == nullptr)
-            result += "( nil )";
+            result += "NIL";
 
         // If the literal is a string
         else if (expr->type == TokenInfo::Type::STRING)
         {
-            result += "( " + *static_cast<std::string *>(expr->value) + " )";
+            result += *static_cast<std::string *>(expr->value);
         }
 
         // If the literal is a number
         else if (expr->type == TokenInfo::Type::NUMBER)
         {
             // Cast to int then convert to string to print
-            result += " " + std::to_string(*static_cast<double *>(expr->value)) + " ";
+            result += std::to_string(*static_cast<double *>(expr->value));
         }
     }
 
@@ -81,18 +84,16 @@ int main(int argc, char *argv[])
 
     // Create Expr = -123 * 45.67 -> Binary ( Unary (-, 123), *, 45.67 )
     // -> ( * ( - 123 ) 45.67 )
-    std::unique_ptr<Expr> binary = std::make_unique<Binary>(
-        std::make_unique<Unary>(Token(TokenInfo::MINUS, "-", nullptr, 1),
-                                std::make_unique<Literal>((void *)new double(123), TokenInfo::Type::NUMBER)),
-        Token(TokenInfo::STAR, "*", nullptr, 1),
-        std::make_unique<Literal>((void *)new double(45.67), TokenInfo::Type::NUMBER));
+    /* std::unique_ptr<Expr> binary = std::make_unique<Binary>( std::make_unique<Unary>(Token(TokenInfo::MINUS, "-",
+     * nullptr, 1), std::make_unique<Literal>((void *)new double(123), TokenInfo::Type::NUMBER)), Token(TokenInfo::STAR,
+     * "*", nullptr, 1), std::make_unique<Literal>((void *)new double(45.67), TokenInfo::Type::NUMBER)); */
 
     // Very simple expression: 2 + 3 -> Binary ( +, 2, 3)
     // -> ( + 2 3 )
-    /* std::unique_ptr<Expr> binary = */
-    /*     std::make_unique<Binary>(std::make_unique<Literal>((void *)new double(2), TokenInfo::Type::NUMBER), */
-    /*                              Token(TokenInfo::PLUS, "+", nullptr, 1), */
-    /*                              std::make_unique<Literal>((void *)new double(3), TokenInfo::Type::NUMBER)); */
+    std::unique_ptr<Expr> binary =
+        std::make_unique<Binary>(std::make_unique<Literal>((void *)new double(2), TokenInfo::Type::NUMBER),
+                                 Token(TokenInfo::PLUS, "+", nullptr, 1),
+                                 std::make_unique<Literal>((void *)new double(3), TokenInfo::Type::NUMBER));
 
     // In the Printer class, set the print result to the binary expression
     printer.setPrintResult(binary);
